5e1b6d539b2bd3d4a97364f722b6bfbec263c111
dianbhuiza <diandev03@gmail.com>
2025-12-18 23:39:58 -0500

    feat: import ConfigModule

--- apps/products-ms/src/app.module.ts
+++ apps/products-ms/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import z from 'zod';
+import { envSchema } from './config/env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env',
+      validationSchema: envSchema,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
