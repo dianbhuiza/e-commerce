f0bcc18b5ce8e10a2d43b2b4cf901152379b7a79
dianbhuiza <diandev03@gmail.com>
2025-12-18 23:47:23 -0500

    refactor: delete unnecessary service and controller

--- apps/products-ms/src/app.module.ts
+++ apps/products-ms/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import z from 'zod';
 import { envSchema } from './config/env';
